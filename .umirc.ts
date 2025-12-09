efbe03d018ef964e5321837bc689b1aa87da8348
刘春达 <liuchunda@xiaomi.com>
2025-12-09 19:24:13 +0800

    feat: 部署开发

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "umi";
 export default defineConfig({
+  esbuildMinifyIIFE: true,
   routes: [
     { path: "/", component: "@/pages/index" ,redirect: "/docs"},
     { path: "/docs", component: "@/pages/docs" },
